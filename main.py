b38c2a3b6e8da8de8ea732fe155f0a8941b70ebb
jrbella <jbella@icsars.org>
2023-03-04 20:59:45 -0600

    updating quick sample work

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
-@app.get("/index")
+@app.get("/hermes/index")
 def index():
     return {"message": "Welcome FastAPI Nerds"}
 
 # get login
-@app.get("/login/")
+@app.get("/hermes/login/")
 def login(username: str, password:str):
     if valid_users.get(username) ==  None:
         return {"message" : "user does not exist"}
@@ -18,9 +17,9 @@ def login(username: str, password:str):
             return user
         else:
             return {"message": "invalid user"}
-
+'''
 # register or signup
-@app.post("/login/signup")
+@app.post("/hermes/login/register")
 def register(uname: str, passwd: str):
     if(uname == None and passwd == None):
         return {"message": "user exists"}
@@ -31,7 +30,7 @@ def register(uname: str, passwd: str):
         pending_users[uname]= user
         return user
     
-@app.put("/account/profile/update/{username}")
+@app.put("/hermes/account/profile/update/{username}")
 def update_profile(username: str, id: UUID, new_profile: UserProfile):
     if valid_users.get(username) == None:
         return {"message" : "user does not exist"}
@@ -43,3 +42,31 @@ def update_profile(username: str, id: UUID, new_profile: UserProfile):
         else:
             return {"message": "user does not exist"}
 
+@app.patch("/hermes/account/profile/update/names/{username}")
+def update_profile_names(username: str, id: UUID, new_names: Dict[str, str]):
+    if valid_users.get(username == None):
+        return {"message": "user does not exist"}
+    elif new_names == None:
+        return {"message": "new names are required"}
+    else:
+        user = valid_users.get(username)
+        if user.id == id:
+            profile = valid_profiles[username]
+            profile.firstname = new_names['fname']
+            profile.lastname = new_names['lname']
+            profile.middle_initial = new_names['mi']
+            valid_profiles[username] = profile
+            return {"message": "successfully updated"}
+        else:
+            return {"message": "user does not exist"}
+
+@app.delete("/hermes/discussion/posts/remove/{username}")
+def delete_discussion(username: str, id: UUID):
+    if valid_users.get(username) == None:
+        return {"message": "user does not exist"}
+    elif discussion_posts.get(id) == None:
+        return {"message": "post does not exist"}
+    else:
+        del discussion_posts[id]
+        return {"message": "main post deleted"}
+'''
